2aa4ba702c24a18e54dc70f16502b72b0a80be2b
Jonathan Glines <auntieNeo@gmail.com>
2011-08-28 20:50:40 -0600

    Implemented insertion sort in ruby. Removed physics crap.

---
+++ cs385/insertion_sort_demo.rb
@@ -0,0 +1,60 @@
+#!/usr/bin/ruby
+# param a is an Array of numbers
+class Array
+  def to_s
+    self.join ','
+  end
+end
+
+def insertion_sort a
+  # a[0], being a single number, is sorted
+
+  n = a.size
+  # Assume a[0]..a[j-1] is sorted in
+  # nondescending order before a run of the loop.
+  # The "index j" run of the loop will insert a[j]
+  # in a[0]..a[j-1].
+  1.upto(n-1) do |j|
+    puts "looping with j = #{j}"
+    key = a[j]
+    # Push elements that are bigger than "key" to the right
+    # to make room for inserting "key"
+    # Note that since a[0]..a[j-1] is sorted, we can just
+    # stop when we see a element no larger than "key."
+    k = j-1
+    puts "k = #{k}"
+    while k >= 0 and a[k] > key
+      a[k+1] = a[k]
+      k = k-1
+    end # while
+    # Since k has been decremented, a[k+1] is the element
+    # to be overwritten by the key
+    a[k+1] = key
+    puts "a = #{a}"
+  end # do
+  return a
+end # def insertion_sort
+
+def selection_sort a
+  0.upto(a.size - 2) { |i|
+    puts  "i = #{i}"
+    # find the least value element from index i onward
+    least = i
+    j = i+1
+    j.upto(a.size - 1) { |j|
+      puts "j = #{j}"
+      if a[j] < a[least] then
+        least = j
+      end
+    }
+    # swap the element at index i with the element at index least
+    temp = a[i]
+    a[i] = a[least]
+    a[least] = temp
+  }
+end
+
+a = [4, 3, -5, 2, 7, 67, 57, -64, -34, 2, 0]
+#insertion_sort a
+selection_sort a
+puts a.to_s
