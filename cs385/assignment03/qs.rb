1dd129a483418ccd87735c2c59e616c5bf99acc5
Jonathan Glines <auntieNeo@gmail.com>
2011-10-04 11:17:18 -0600

    Changed quicksort to use the partition method.

--- cs385/assignment03/qs.rb
+++ cs385/assignment03/qs.rb
@@ -7,36 +7,17 @@ class Array
   def qs(left, right)
     if left < right then
       # partition step
-      pivot = self[right];  # pick an arbitrary pivot value
-      i = left - 1;  # i indexes the lower partition
-      j = left;  # j indexes the upper partition
-      left.upto(right - 1) do |j|
-        if(self[j] <= pivot) then
-          # swap the element at index j into the lower partition, and increment both i and j
-          foo = self[i + 1];
-          self[i + 1] = self[j];
-          self[j] = foo;
-          i = i + 1;
-          j = j + 1;
-        else
-          # leave the element at index j in the upper partition, and increment j
-          j = j + 1;
-        end
-      end
-      # swap the pivot element so that it lies between the two partitions
-      foo = self[i + 1];
-      self[i + 1] = self[right];
-      self[right] = foo;
+      pivot = partition(left, right);
 
       # recursive step
-      qs(left, i);
-      qs(i + 2, right);
+      qs(left, pivot - 1);
+      qs(pivot + 1, right);
     end
   end
 
   public
   def quickselect(k)
-    u = self.uniq;
+    u = self.uniq;  # search from the list of unique elements
     return u.qselect(0, u.length - 1, k)
   end
 
@@ -45,6 +26,7 @@ class Array
     if left == right then
       return self[left];
     end
+    # partition the array recursively to find k
     pivot = partition(left, right);
     if pivot == k then
       return self[k];
@@ -55,7 +37,7 @@ class Array
     end
   end
 
-  public
+  private
   def partition(left, right)
     pivot = self[right];  # pick an arbitrary pivot value # TODO: should be more random than this
     i = left - 1;  # i indexes the lower partition
@@ -81,5 +63,4 @@ class Array
     # return the pivot index between the partitions
     return i + 1;
   end
-
 end
