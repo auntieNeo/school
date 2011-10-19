c426fe8482772293022a903bcd2c2f35e3dc19ee
Jonathan Glines <auntieNeo@gmail.com>
2011-10-19 17:12:38 -0600

    Got 4 bit adder to work on 7 segment display.

--- ee2275/project7/problem3/main.vhd
+++ ee2275/project7/problem3/main.vhd
@@ -10,9 +10,29 @@ entity main is
 end main;
 
 architecture behavior of main is
+
+component cpg_adder
+  port(cin : in std_logic;
+       a, b : in std_logic_vector(7 downto 0);
+       s : out std_logic_vector(7 downto 0);
+       cout : out std_logic);
+end component;
+
+component bin2hex
+  port(bin : in std_logic_vector(3 downto 0);
+       hex : out std_logic_vector(6 downto 0));
+end component;
+
+signal sum : std_logic_vector(7 downto 0);
+alias sum0 : std_logic_vector(3 downto 0) is sum(3 downto 0);
 signal d0, d1, d2, d3 : std_logic_vector(6 downto 0);
 signal counter : std_logic_vector(12 downto 0);
 begin
+
+  adder: cpg_adder port map (cin => '0', a(0) => sw(0), a(1) => sw(1), a(2) => sw(2), a(3) => sw(3), a(4) => '0', a(5) => '0', a(6) => '0', a(7) => '0', b(0) => sw(4), b(1) => sw(5), b(2) => sw(6), b(3) => sw(7), b(4) => '0', b(5) => '0', b(6) => '0', b(7) => '0', s => sum);
+  hex0: bin2hex port map (bin => sum0, hex => d0);
+  hex1: bin2hex port map (bin(0) => sum(4), bin(1) => sum(5), bin(2) => sum(6), bin(3) => sum(7), hex => d1);
+
   p: process(clock) is
   begin
     if (rising_edge(clock)) then
@@ -37,8 +57,6 @@ begin
       end if;
     end if;
   end process;
-  d0 <= "1100000";
-  d1 <= "1100010";
-  d2 <= "1100010";
-  d3 <= "1100000";
+  d2 <= "1111111";
+  d3 <= "1111111";
 end behavior;
