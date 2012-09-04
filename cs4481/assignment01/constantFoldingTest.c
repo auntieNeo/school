031c21a67f858f59cf5e76c9c03ad801852861ac
Jonathan Glines <auntieNeo@gmail.com>
2012-09-04 16:18:12 -0700

    Added tests for problems 2 and 3.

---
+++ cs4481/assignment01/constantFoldingTest.c
@@ -0,0 +1,11 @@
+int main(int argc, char **argv)
+{
+  int x, y, z;
+
+  x = 40 + 2;
+
+  y = 4;
+  z = y + 2;
+
+  return 0;
+}
---
+++ cs4481/assignment01/errorTest.c
@@ -0,0 +1,12 @@
+int main(int argc, char **argv)
+{
+  const char *a = "blah";
+  int i = 2;
+
+//  (42 - );
+//  a[i);
+//  42 = i;
+  42.factor.();
+
+  return 0;
+}
