cda75fd42407c7212b5b6682832b861dfc229eeb
Jonathan Glines <auntieNeo@gmail.com>
2012-09-12 14:01:16 -0700

    Added file for S-DES implementation.

---
+++ cs4420/assignment02/sdes.c
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+
+void sdes_encrypt(unsigned char *plaintext, size_t plaintext_blocks, const char *key);
+void sdes_decrypt(unsigned char *ciphertext, size_t ciphertext_blocks, const char *key);
+
+int main(int argc, char **argv)
+{
+}
